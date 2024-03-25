4f16f1e0b78ecd264064254d2c87bf6974e5c332
pheonix0723 04 <pheonix.squadron07@gmail.com>
2024-03-25 13:19:15 +0000

    Chat functionality is added

--- app/pages/4_Homepage.py
+++ app/pages/4_Homepage.py
@@ -36,11 +36,17 @@ else:
 
 
 # Initial setup of the Homepage
-st.sesion_state['use_history'] = False
+st.session_state['use_history'] = False
 dotenv.load_dotenv("/workspace/LEARN_SMART/Secrets/.env")
 bucket_name = os.getenv("S3_BUCKET_NAME")
-local_files_path = "/workspace/LEARN_SMART/docs/"
+local_files_path = "/workspace/LEARN_SMART/app/docs/"
 os.makedirs(f"{local_files_path}", exist_ok = True)
+
+
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
 
 
 # Function to get the current time
@@ -87,8 +93,30 @@ def download_files_from_s3_folder(bucket_name, folder_prefix, local_directory):
 
 
 
+
+def append_message(container, role, content):
+    container.session_state.chat_history.append({"role": role, "content": content})
+
+
+
+def get_history():
+    with open("/workspace/LEARN_SMART/app/docs/chat.json", "r") as file:
+        data = json.load(file)
+        for message in data:
+            st.session_state.chat_history.append({"role":f"{messages["role"]}", "content":f"{messages["content"]}"})
+
+
+
+def display_chat_history(container):
+    for message in st.session_state.chat_history:
+        with container.chat_message(message["role"]):
+            container.markdown(message["content"])
+
+
 # Specifying the columns for the chat history and current history.
 prev_chats, cur_chat = st.columns(spec = [0.3,0.7], gap="small")
+
+
 
 
 with prev_chats:
@@ -99,31 +127,52 @@ with prev_chats:
         for folder_name in folders_list:
             if prev_chats.button(f'{folder_name}'):
                 # st.write(f"{folder_name}")
+                st.session_state.chat_history = []
                 chat_folder = f"users/{username}/{folder_name}/"
                 download_files_from_s3_folder(bucket_name, chat_folder, local_files_path)
-                st.session_state['use_history'] = True
-                # break
-                st.write("Files have been downloaded")
+                # st.session_state['use_history'] = True
+                get_history()
     else:
+        with open("/workspace/LEARN_SMART/app/docs/chat.json", "w") as file:
+            pass
         st.write("You don't have any previous chats!!")
-        st.session_state['use_history'] = False
+        # st.session_state['use_history'] = False
+
+
 
 with cur_chat:
-    if st.session_state['use_history']:
-        # Display the previous chat
-        # Chat
-        # Update the json
-    else:
-        # Create a new json file
-        # Chat
-        # Update the json.
-    # If end chat:
-        # Get current time
-        # if not st.session_stat['use_history']:
-            # Create a new folder in s3 bucket.
-        # Else:
-            # Rename the folder with current datetime
-    # Upload the files in the s3 bucket's folder
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
+    if prompt := st.chat_input("User prompt: ", max_chars = 500):
+        li = []
+        li.append({"role":"user", "content":prompt})
+        response = "Reponse"
+        li.append({"role":"assistant", "content":response})
+        st.session_state.chat_history.append(li)
+
+    for message in reversed(st.session_state.chat_history):
+        for i in range(2):
+            st.chat_message(message[i]['role']).markdown(message[i]['content'])
+
+    # display_chat_history(cur_chat)
+
+
+    #     # Display the previous chat
+    #     # Chat
+    #     # Update the json
+    # else:
+    #     # Create a new json file
+    #     # Chat
+    #     # Update the json.
+    # # If end chat:
+    #     # Get current time
+    #     # if not st.session_stat['use_history']:
+    #         # Create a new folder in s3 bucket.
+    #     # Else:
+    #         # Rename the folder with current datetime
+    # # Upload the files in the s3 bucket's folder
+
     
 
     st.header("Another cat")
