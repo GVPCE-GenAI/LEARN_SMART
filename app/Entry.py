0cad67d80768893ca13083e9bff474f70f629efb
pheonix0723 <136866319+pheonix0723@users.noreply.github.com>
2024-03-27 12:41:37 +0530

    Update Entry.py

--- app/Entry.py
+++ app/Entry.py
@@ -38,31 +38,9 @@ def main():
         }
         </style>
         """, unsafe_allow_html=True)
-    
-    # st.markdown(
-    #     """
-    #     <div style="margin-top: 20px;">
-    #         <a href="pages/1_Login.py" class="button" style="display: inline-block; padding: 10px 20px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Login</a>
-    #     </div>
-    #     """,
-    #     unsafe_allow_html=True
-    # )
-
-    # st.markdown(
-    #     """
-    #     <div style="margin-top: 20px;">
-    #         <a href="pages/2_Signup.py" class="button" style="display: inline-block; padding: 10px 20px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Sign Up</a>
-    #     </div>
-    #     """,
-    #     unsafe_allow_html=True
-    # )
 
     st.page_link("pages/1_Login.py", label=":point_up_2: Login", help="For your existing account!")
     st.page_link("pages/2_Signup.py", label=":male-technologist: Sign up", help="To create a new account!")
 
 if __name__ == "__main__":
     main()
-
-# # Either SignUp for a new or Login to existing account
-# st.page_link("pages/1_Login.py", label="Login to your existing account!")
-# st.page_link("pages/2_Signup.py", label="Sign up for a new account!")
